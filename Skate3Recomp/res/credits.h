5b10afa7cb72634df377d2e4f4b18fc503f78c68
Ricardo Baucells <ricardo@baucells.net>
2026-08-14 22:08:14 -0400

    add version stuff and add my self to credits

---
+++ Skate3Recomp/res/credits.h
@@ -0,0 +1,23 @@
+#pragma once
+
+inline std::array<const char*, 17> g_credits =
+{
+    "Skyth",
+    "Sajid",
+    "Hyper",
+    "Darío",
+    "DeaThProj",
+    "RadiantDerg",
+    "PTKay",
+    "SuperSonic16",
+    "NextinHKRY",
+    "LadyLunanova",
+    "LJSTAR",
+    "saguinee",
+    "Goalringmod27",
+    "RagdollClash",
+    "DaGuAr",
+    "brianuuuSonic",
+    "Kitzuku",
+    "rbaucells"
+};
